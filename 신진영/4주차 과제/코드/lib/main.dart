d7261e7d46ac6c651d9359ba7be9bf4bbcb5e72b
Nyeong <iwbm312@gmail.com>
2023-05-03 21:39:21 +0900

    Feat: week4 assignment

---
+++ 신진영/4주차 과제/코드/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:run_day/screen/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'kangwon',
+      textTheme: TextTheme(
+        headline1: TextStyle(
+          color: Colors.black,
+          fontSize: 50.0,
+          fontWeight: FontWeight.w700,
+          fontFamily: 'jalnan',
+        ),
+        headline2: TextStyle(
+          color: Colors.black,
+          fontSize: 40.0,
+          fontWeight: FontWeight.w700,
+        ),
+        bodyText1: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0
+        ),
+        bodyText2: TextStyle(
+          color: Colors.black,
+          fontSize: 20.0,
+        ),
+      )
+    ),
+    home: HomeScreen(),
+  ));
+}
